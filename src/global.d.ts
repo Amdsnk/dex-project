ceba3d30362af5cd5fd523d3f90a5b7df70ad08e
Amdsnk <75839443+Amdsnk@users.noreply.github.com>
2024-10-22 22:13:00 +0700

    Update global.d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+// global.d.ts
+interface Ethereum {
+  isMetaMask?: boolean;
+  request: (args: { method: string; params?: any[] }) => Promise<any>;
+  on: (event: string, callback: (...args: any[]) => void) => void;
+  removeListener: (event: string, callback: (...args: any[]) => void) => void;
+}
+
+interface Window {
+  ethereum?: Ethereum;
+}
